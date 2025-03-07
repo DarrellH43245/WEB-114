8abdb214ca3be8ec2841042c66ecc6192f4b2aab
DarrellH43245 <dholtz1@stumail.jccc.edu>
2025-03-06 18:26:52 -0600

    Project files

---
+++ me.js
@@ -0,0 +1,12 @@
+// Darrell Holtz 3-5-2025
+'use strict';
+
+let name = "Darrell (closer family and friends refer to me as 'Deano')";
+let favoriteColor = "Blue";
+let hobby = "Playing Video Games";
+let favoriteFood = "Steak";
+
+console.log(name);
+console.log(favoriteColor);
+console.log(hobby);
+console.log(favoriteFood);
